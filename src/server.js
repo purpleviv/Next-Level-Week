775cfe0554a8c6c934b305d878929cafc9394aac
airotiv <vitoriarodriguessilva@gmail.com>
2020-08-06 22:40:21 -0300

    Reorganização do código em funções (server.js)

--- src/server.js
+++ src/server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const server = express();
 
+function pageLanding(req, res) {
+  return res.sendFile(__dirname + "/views/index.html");
+}
+
+function pageStudy(req, res) {
+  return res.sendFile(__dirname + "/views/study.html");
+}
+
+function pageGiveClasses(req, res) {
+  return res.sendFile(__dirname + "/views/give-classes.html");
+}
+
 server
   .use(express.static("public"))
-  .get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html");
-  })
-  .get("/study", (req, res) => {
-    return res.sendFile(__dirname + "/views/study.html");
-  })
-  .get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html");
-  })
+  .get("/", pageLanding)
+  .get("/study", pageStudy)
+  .get("/give-classes", pageGiveClasses)
   .listen(5000);
 //Para mostrar o valor de __dirname
 // console.log(__dirname)
