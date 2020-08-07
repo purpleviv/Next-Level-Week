f84e8e379e670515c36e27cd343de04b4fd38950
airotiv <vitoriarodriguessilva@gmail.com>
2020-08-06 23:38:02 -0300

    Configurando e utilizando nunjucks para acessar as paginas

--- src/server.js
+++ src/server.js
@@ -1,20 +1,30 @@
-const express = require("express");
-const server = express();
-
 function pageLanding(req, res) {
-  return res.sendFile(__dirname + "/views/index.html");
+  return res.render("index.html");
 }
 
 function pageStudy(req, res) {
-  return res.sendFile(__dirname + "/views/study.html");
+  return res.render("study.html");
 }
 
 function pageGiveClasses(req, res) {
-  return res.sendFile(__dirname + "/views/give-classes.html");
+  return res.render("give-classes.html");
 }
 
-server
+const express = require("express");
+const server = express();
+
+// configurar nunjucks
+// argumentos: pasta com arquivos html, enviar objetos com alguma coisa
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
+});
+
+server // configurar arquivos estáticos (css, scripts, imagens)
   .use(express.static("public"))
+
+  // rotas da aplicação
   .get("/", pageLanding)
   .get("/study", pageStudy)
   .get("/give-classes", pageGiveClasses)
